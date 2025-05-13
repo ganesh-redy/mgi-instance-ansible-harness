168886579a957d12ef46d353608f7d904cce0b41
ganesh-redy <ganeshreddy7610@gmail.com>
2025-05-13 16:02:11 +0530

    Update mig.tf

--- mig.tf
+++ mig.tf
@@ -4,20 +4,13 @@ provider "google" {
   zone        = "us-central1-a"
 }
 
-# Step 1: Generate SSH Key
-resource "tls_private_key" "my_ssh_key" {
-  algorithm = "RSA"
-  rsa_bits  = 4096
-}
-
-# Step 2: Instance Template with SSH key in metadata
 resource "google_compute_instance_template" "temp1" {
   name         = "template1"
   machine_type = "e2-standard-2"
 
   disk {
-    boot         = true
-    auto_delete  = true
+    boot        = true
+    auto_delete = true
 
     initialize_params {
       source_image = "centos-stream-9"
@@ -29,11 +22,12 @@ resource "google_compute_instance_template" "temp1" {
   }
 
   metadata = {
-    ssh-keys = "ansible:${tls_private_key.my_ssh_key.public_key_openssh}"
+    ssh-keys = "ansible:${file("/root/.ssh/id_rsa.pub")}"
   }
+
+  tags = ["harnessvms"]
 }
 
-# Step 3: Health Check
 resource "google_compute_health_check" "name" {
   name = "health1"
 
@@ -48,7 +42,6 @@ resource "google_compute_health_check" "name" {
   check_interval_sec  = 10
 }
 
-# Step 4: Managed Instance Group with 2 VMs
 resource "google_compute_instance_group_manager" "mange" {
   name               = "instance-manger-1"
   base_instance_name = "okay"
@@ -64,14 +57,4 @@ resource "google_compute_instance_group_manager" "mange" {
     health_check      = google_compute_health_check.name.self_link
     initial_delay_sec = 300
   }
-}
-
-# Step 5: Outputs
-output "private_key" {
-  value     = tls_private_key.my_ssh_key.private_key_pem
-  sensitive = true
-}
-
-output "public_key" {
-  value = tls_private_key.my_ssh_key.public_key_openssh
 }
